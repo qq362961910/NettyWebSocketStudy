06b8f1aa2d1d2a7d157d28ec5c276aee851e8ef0
yangjian <362961910@qq.com>
2017-02-15 16:13:15 +0800

    添加java客户端test Demo

---
+++ pusher-client/src/test/java/ChatBot.java
@@ -0,0 +1,50 @@
+import java.io.StringReader;
+import java.net.URI;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+/**
+ * ChatBot
+ * @author Jiji_Sasidharan
+ */
+public class ChatBot {
+
+    /**
+     * main
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        final ChatClientEndpoint clientEndPoint = new ChatClientEndpoint(new URI("ws://192.168.14.62:8888"));
+        clientEndPoint.addMessageHandler(new ChatClientEndpoint.MessageHandler() {
+            public void handleMessage(String message) {
+                JsonObject jsonObject = Json.createReader(new StringReader(message)).readObject();
+                String userName = jsonObject.getString("user");
+                if (!"bot".equals(userName)) {
+                    clientEndPoint.sendMessage(getMessage("Hello " + userName +", How are you?"));
+                    // other dirty bot logic goes here.. :)
+                }
+            }
+        });
+
+        while (true) {
+            clientEndPoint.sendMessage(getMessage("Hi There!!"));
+            Thread.sleep(30000);
+        }
+    }
+
+    /**
+     * Create a json representation.
+     *
+     * @param message
+     * @return
+     */
+    private static String getMessage(String message) {
+        return Json.createObjectBuilder()
+                .add("user", "bot")
+                .add("message", message)
+                .build()
+                .toString();
+    }
+}
---
+++ pusher-client/src/test/java/ChatClientEndpoint.java
@@ -0,0 +1,90 @@
+import org.glassfish.tyrus.client.ClientManager;
+
+import javax.websocket.*;
+import java.net.URI;
+
+/**
+ * ChatServer Client
+ *
+ * @author Jiji_Sasidharan
+ */
+@ClientEndpoint
+public class ChatClientEndpoint {
+    Session userSession = null;
+    private MessageHandler messageHandler;
+
+    public ChatClientEndpoint(URI endpointURI) {
+        try {
+            ClientManager container = (ClientManager)ContainerProvider
+                    .getWebSocketContainer();
+            container.connectToServer(this, endpointURI);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Callback hook for Connection open events.
+     *
+     * @param userSession
+     *            the userSession which is opened.
+     */
+    @OnOpen
+    public void onOpen(Session userSession) {
+        this.userSession = userSession;
+    }
+
+    /**
+     * Callback hook for Connection close events.
+     *
+     * @param userSession
+     *            the userSession which is getting closed.
+     * @param reason
+     *            the reason for connection close
+     */
+    @OnClose
+    public void onClose(Session userSession, CloseReason reason) {
+        this.userSession = null;
+    }
+
+    /**
+     * Callback hook for Message Events. This method will be invoked when a
+     * client send a message.
+     *
+     * @param message
+     *            The text message
+     */
+    @OnMessage
+    public void onMessage(String message) {
+        if (this.messageHandler != null)
+            this.messageHandler.handleMessage(message);
+    }
+
+    /**
+     * register message handler
+     *
+     * @param message
+     */
+    public void addMessageHandler(MessageHandler msgHandler) {
+        this.messageHandler = msgHandler;
+    }
+
+    /**
+     * Send a message.
+     *
+     * @param user
+     * @param message
+     */
+    public void sendMessage(String message) {
+        this.userSession.getAsyncRemote().sendText(message);
+    }
+
+    /**
+     * Message handler.
+     *
+     * @author Jiji_Sasidharan
+     */
+    public static interface MessageHandler {
+        public void handleMessage(String message);
+    }
+}
